847c26ef6a3260bd215f84ede43986d137c9d010
Elve Johansson <elve2002@live.se>
2023-07-23 19:06:48 +0200

    treesitter and lsp

--- nvim/.config/nvim/lua/_packer.lua
+++ nvim/.config/nvim/lua/_packer.lua
@@ -21,6 +21,29 @@ return require('packer').startup(function(use)
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.api.nvim_command, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
+
   if packer_bootstrap then
     require('packer').sync()
   end
