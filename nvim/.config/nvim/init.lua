7ba8e75429a2f34c2add8d77e1c244f254af84a3
Elve Johansson <elve2002@live.se>
2024-01-16 00:04:44 +0100

    added tpope/vim-sleuth, bumped telescope to 0.1.5

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,120 +1,110 @@
 local global = vim.g
+local opt = vim.o
 
 global.mapleader = " "
 global.maplocalleader = " "
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nglobal.t",
-    "--branch=stable",     -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nglobal.t",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "mellow-theme/mellow.nvim",
-    name = "mellow",
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme mellow]])
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = "kyazdani42/nvim-web-devicons",
-    config = true,
-    opts = {}
+    "tpope/vim-sleuth",
+    {
+        "mellow-theme/mellow.nvim",
+        name = "mellow",
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme mellow]])
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = "kyazdani42/nvim-web-devicons",
+        config = true,
+        opts = {}
 
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-      vim.keymap.set("n", "<leader>fs", function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end)
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>fs", function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
 
-      vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "json",
-          "html",
-          "css",
-          "typescript",
-          "javascript",
-          "rust",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "regex",
-          "yaml",
-        },
-        auto_install = true, -- Needs `tree-sitter` CLI locally
+            vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "json",
+                    "html",
+                    "css",
+                    "typescript",
+                    "javascript",
+                    "rust",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "regex",
+                    "yaml",
+                },
+                auto_install = true, -- Needs `tree-sitter` CLI locally
 
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
-  },
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    },
 })
 
-local opt = vim.o
 
--- [[ Line rendering ]]
--- opt.colorcolumn = "80"  -- Show column for max line length
-opt.number = true -- Show line numbers
+-- [[ Options ]]
+opt.number = true         -- Show line numbers
 opt.relativenumber = true -- Show relative line numbers
-opt.scrolloff = 999 -- No. of context line around cursor
-opt.signcolumn = "no" -- Should we show the signcolumn?
-
--- [[ Filetypes ]]
-opt.encoding = "utf8" -- Set character encoding
+opt.scrolloff = 999       -- No. of context line around cursor
+opt.encoding = "utf8"     -- Set character encoding
 opt.fileencoding = "utf8" -- Set file endcoding
-
--- [[ Theme/colors ]]
-opt.syntax = "ON" -- Enable syntax highlighting
-opt.termguicolors = true -- Enable 24-bit color in TUI
-
--- [[ Search ]]
-opt.ignorecase = true -- Ignore casing in search pattern
-opt.smartcase = true -- Override ignorecase if search contains capitals
-opt.incsearch = true -- Turn on incremental search
-opt.hlsearch = true -- Hightlight search matches
-
--- [[ Whitespace ]]
-opt.expandtab = true -- Use spaces instead of tabs
-opt.shiftwidth = true -- Set size of indentation
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.softtabstop = 2 -- -||-, but for insert mode
-opt.autoindent = true -- Use indent from previous line
-opt.smartindent = true -- Use "smart" (aka C) indent
+opt.syntax = "ON"         -- Enable syntax highlighting
+opt.termguicolors = true  -- Enable 24-bit color in TUI
+opt.ignorecase = true     -- Ignore casing in search pattern
+opt.smartcase = true      -- Override ignorecase if search contains capitals
+opt.incsearch = true      -- Turn on incremental search
+opt.hlsearch = true       -- Hightlight search matches
+opt.expandtab = true      -- Use spaces instead of tabs
+opt.shiftwidth = true     -- Set size of indentation
+opt.tabstop = 2           -- Number of spaces tabs count for
+opt.softtabstop = 2       -- -||-, but for insert mode
+opt.autoindent = true     -- Use indent from previous line
+opt.smartindent = true    -- Use "smart" (aka C) indent
 
 
 -- [[ Hard mode ]]
@@ -129,14 +119,4 @@ vim.keymap.set("i", "<right>", "<nop>")
 
 -- [[ Keybinds ]]
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
-
-
-
-
-
-
-
-
-
-
 
