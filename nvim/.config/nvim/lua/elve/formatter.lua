7a93ad1d89ac2a05e8f5563ba6b9d25917a24528
Elve Johansson <elve2002@live.se>
2024-06-06 19:44:37 +0200

    disable format on save for neoformat

--- nvim/.config/nvim/lua/elve/formatter.lua
+++ nvim/.config/nvim/lua/elve/formatter.lua
@@ -3,8 +3,10 @@
 --  autocmd BufWritePre * undojoin | Neoformat
 --augroup END
 
-local au_id = vim.api.nvim_create_augroup("Neoformat", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-    command = "Neoformat",
-    group = au_id,
-})
+--vim.g.neoformat_try_node_exe = 1
+
+--local au_id = vim.api.nvim_create_augroup("Neoformat", { clear = true })
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--    command = "Neoformat",
+--    group = au_id,
+--})
