b416a87a0ce6938504bf040d20b8d0947a068079
Elve Johansson <elve2002@live.se>
2024-07-19 00:06:23 +0200

    autopair+autotag, update plugins

---
+++ nvim/.config/nvim/lua/elve/plugins/autopairs.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    enable_close = true,
+                    enable_rename = true,
+                    enable_close_on_slash = false
+                }
+            })
+        end
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true
+    }
+}
